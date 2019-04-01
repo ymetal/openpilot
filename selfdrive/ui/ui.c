4e40b6e27d39bec87ac3be4e5d5ef40861a254d1
Shane <shane@smiskol.com>
2019-03-31 21:13:43 -0500

    Screen off mod

--- selfdrive/ui/ui.c
+++ selfdrive/ui/ui.c
@@ -268,13 +268,18 @@ static void set_brightness(UIState *s, int brightness) {
 static void set_awake(UIState *s, bool awake) {
   if (awake) {
     // 30 second timeout at 30 fps
-    s->awake_timeout = 30*30;
+    if (s->b.tri_state_switch < 3) {
+      s->awake_timeout = 30*30;
+    } else {
+      s->awake_timeout = 3*30;
+    }
   }
   if (s->awake != awake) {
     s->awake = awake;
 
     if (awake) {
       LOG("awake normal");
+      set_brightness(s, 150);
       framebuffer_set_power(s->fb, HWC_POWER_MODE_NORMAL);
     } else {
       LOG("awake off");
