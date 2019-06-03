623ef3d9371e3b8aae2beece7d63b7e44617d76a
Shane <shane@smiskol.com>
2019-06-03 05:13:19 -0500

    test

--- selfdrive/df/lib_main.py
+++ selfdrive/df/lib_main.py
@@ -1,21 +1,10 @@
 from cffi import FFI
 import os
-import subprocess
-
 
 if not os.path.isfile('/data/openpilot/selfdrive/df/d_f.so'):
     Makefile = "/data/openpilot/selfdrive/df/Makefile.sh"
     os.chmod(Makefile, 0775)
     os.system("exec " + Makefile)
-
-
-'''mpc_dir = "/data/openpilot/selfdrive/df"
-
-if platform.machine() == "x86_64":
-  try:
-    FFI().dlopen(os.path.join(mpc_dir, "main"))
-  except OSError:
-    print("Error!!")'''
 
 #subprocess.check_call(["make", "-j4"], cwd=mpc_dir)
 
@@ -34,14 +23,3 @@ def get_libmpc():
     """)
 
     return (ffi, ffi.dlopen(libmpc_fn))
-
-'''mpcs = [_get_libmpc(1), _get_libmpc(2)]
-
-def get_libmpc(mpc_id):
-    return mpcs[mpc_id - 1]'''
-
-
-'''start=time.time()
-output = subprocess.check_output(['/data/openpilot/selfdrive/df/main', '.8', '.5', '.2', '.4', '.5'])
-print(float(output))
-print(time.time()-start)'''
--- selfdrive/df/main.py
+++ selfdrive/df/main.py
@@ -1,9 +1,5 @@
 import time
-#from ctypes import *
-#import _ctypes
-#lib1 = cdll.LoadLibrary("/data/openpilot/selfdrive/df/libs/libSNPE.so")
 from selfdrive.df import lib_main
-#import os
 ffi, libmpc = lib_main.get_libmpc()
 libmpc.init_model()
 start = time.time()
