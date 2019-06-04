6fc1c80f3d9ad9c0090880ee705e24ec8f46593d
Shane <shane@smiskol.com>
2019-06-04 15:52:11 -0500

    fix

--- selfdrive/df/lib_main.py
+++ selfdrive/df/lib_main.py
@@ -7,7 +7,7 @@ Makefile = "/data/openpilot/selfdrive/df/Makefile"
 subprocess.check_call(["make", "-j4"], cwd="/data/openpilot/selfdrive/df")
 
 def get_libmpc():
-    libmpc_fn = "/data/openpilot/selfdrive/df/dynamic_follow.o"
+    libmpc_fn = "/data/openpilot/selfdrive/df/dynamic_follow.so"
 
     ffi = FFI()
     ffi.cdef("""
