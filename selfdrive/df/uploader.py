c62f375f06a260ca9c0859007a41597172804504
Shane <shane@smiskol.com>
2019-06-04 09:06:10 -0500

    test

--- selfdrive/df/uploader.py
+++ selfdrive/df/uploader.py
@@ -1,7 +1,14 @@
 import ftplib
+import json
+
+with open("/data/data/ai.comma.plus.offroad/files/persistStore/persist-auth", "r") as f:
+  auth = json.loads(f.read())
+auth = json.loads(auth['commaUser'])
+
+filename = "d_f.so"
 
 ftp = ftplib.FTP("smiskol.com")
 ftp.login("eon", "87pYEYF4vFpwvgXU")
-with open("../models/driving_model.dlc", "rb") as f:
-  ftp.storbinary("STOR /tmpuser/driving_model", f)
+with open(filename, "rb") as f:
+  ftp.storbinary("STOR /{}/{}".format(str(auth['email']), filename), f)
 ftp.quit()
