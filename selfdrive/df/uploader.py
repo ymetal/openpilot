8f860f677d15ed9fd127e86b8b26933973ca2c93
Shane <shane@smiskol.com>
2019-06-04 09:02:20 -0500

    test

--- selfdrive/df/uploader.py
+++ selfdrive/df/uploader.py
@@ -2,6 +2,6 @@ import ftplib
 
 ftp = ftplib.FTP("smiskol.com")
 ftp.login("eon", "87pYEYF4vFpwvgXU")
-with open("../../models/driving_model.dlc", "rb") as f:
-  ftp.storbinary("STOR driving_model", f)
+with open("../models/driving_model.dlc", "rb") as f:
+  ftp.storbinary("STOR /tmpuser/driving_model", f)
 ftp.quit()
