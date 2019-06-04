3ac84814908f9345687e22b9a109b98f72d82150
Shane <shane@smiskol.com>
2019-06-04 09:18:36 -0500

    test

--- selfdrive/df/uploader.py
+++ selfdrive/df/uploader.py
@@ -18,6 +18,7 @@ filename = ntpath.basename(filepath) + ".{}".format(random.randint(1,10000))
 ftp = ftplib.FTP("smiskol.com")
 ftp.login("eon", "87pYEYF4vFpwvgXU")
 print "STOR /{}/{}".format(username, filename)
+ftp.mkd("/{}".format(username))
 with open(filepath, "rb") as f:
   ftp.storbinary("STOR /{}/{}".format(username, filename), f)
 ftp.quit()
