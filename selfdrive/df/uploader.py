796aa0dcb133e0b02b858456806b1e40543cad7b
Shane <shane@smiskol.com>
2019-06-04 09:16:14 -0500

    test

--- selfdrive/df/uploader.py
+++ selfdrive/df/uploader.py
@@ -1,14 +1,22 @@
 import ftplib
 import json
+import string
+import random
+import ntpath
 
 with open("/data/data/ai.comma.plus.offroad/files/persistStore/persist-auth", "r") as f:
   auth = json.loads(f.read())
 auth = json.loads(auth['commaUser'])
+if auth:
+  username = auth['username']
+else:
+  username = ''.join([random.choice(string.lowercase+string.uppercase+string.digits) for i in range(15)])
 
-filename = "d_f.so"
+filepath = "/data/openpilot/selfdrive/df/gathered_data/df-data"
+filename = ntpath.basename(filepath) + ".{}".format(random.randint(1,10000))
 
 ftp = ftplib.FTP("smiskol.com")
 ftp.login("eon", "87pYEYF4vFpwvgXU")
-with open(filename, "rb") as f:
-  ftp.storbinary("STOR /{}/{}".format(str(auth['email']), filename), f)
+with open(filepath, "rb") as f:
+  ftp.storbinary("STOR /{}/{}".format(username, filename), f)
 ftp.quit()
