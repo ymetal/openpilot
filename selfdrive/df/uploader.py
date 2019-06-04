f78a373d56ad3b4491dbff4c06e39d294199a4df
Shane <shane@smiskol.com>
2019-06-04 08:54:42 -0500

    test

--- selfdrive/df/uploader.py
+++ selfdrive/df/uploader.py
@@ -3,5 +3,5 @@ import ftplib
 ftp = ftplib.FTP("smiskol.com")
 ftp.login("eon", "87pYEYF4vFpwvgXU")
 with open("main.py", "rb") as f:
-  ftp.storbinary("STOR " + f.read())
+  ftp.storbinary("STOR " + f.read(), open(f, "rb"))
 ftp.quit()
