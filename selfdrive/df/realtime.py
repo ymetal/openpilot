7a95295dddbd50367944d3b2f58b2aee1e47dd47
Shane Smiskol <shane@smiskol.com>
2019-06-06 14:46:26 -0500

    remove

--- selfdrive/df/realtime.py
+++ selfdrive/df/realtime.py
@@ -3,17 +3,6 @@ from selfdrive.services import service_list
 import zmq
 import sys
 
-import time
-f = '%4i %%'
-len_to_clear = len(f)+1
-clear = '\x08'* len_to_clear
-print 'Progress in percent:'+' '*(len_to_clear),
-for i in range(123):
-    print clear+f % (i*100//123),
-    sys.stdout.flush()
-    time.sleep(0.4)
-raw_input('\nDone')
-
 context = zmq.Context()
 poller = zmq.Poller()
 dynamic_follow_sock = messaging.sub_sock(context, service_list['dynamicFollowData'].port, conflate=True, poller=poller)
