9db4c6cd82b5971b399db5ac30495fe50a9dc13e
Shane <shane@smiskol.com>
2019-06-06 12:51:31 -0500

    test

--- selfdrive/df/main.py
+++ selfdrive/df/main.py
@@ -4,6 +4,6 @@ ffi, libmpc = lib_main.get_libmpc()
 libmpc.init_model()
 start = time.time()
 #for i in range(50):
-model_output = libmpc.run_model(0.38932858, 0.24922614, 0.29498888, 0.24680073, 0.34690347)
+model_output = libmpc.run_model(0.38932858, 0.24922614, 0.22, 0.24680073, 0.32690347)
 #print(time.time() - start)
 print(model_output)
