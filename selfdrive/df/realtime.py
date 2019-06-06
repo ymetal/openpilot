8e0a31c18718055a19c4c5890f5a2d387e0082f3
Shane <shane@smiskol.com>
2019-06-06 14:21:26 -0500

    finish realtime debugger

--- selfdrive/df/realtime.py
+++ selfdrive/df/realtime.py
@@ -2,12 +2,11 @@ import selfdrive.messaging as messaging
 from selfdrive.services import service_list
 import zmq
 
-def start():
-  context = zmq.Context()
-  poller = zmq.Poller()
-  dynamic_follow_sock = messaging.sub_sock(context, service_list['dynamicFollowData'].port, conflate=True, poller=poller)
-  while True:
-    dynData = messaging.recv_one(dynamic_follow_sock)
-    if dynData is not None:
-      print("Gas: {}".format(dynData.dynamicFollowData.gas))
-      print("Brake: {}".format(dynData.dynamicFollowData.brake))
+context = zmq.Context()
+poller = zmq.Poller()
+dynamic_follow_sock = messaging.sub_sock(context, service_list['dynamicFollowData'].port, conflate=True, poller=poller)
+while True:
+  dynData = messaging.recv_one(dynamic_follow_sock)
+  if dynData is not None:
+    print("Gas: {}".format(dynData.dynamicFollowData.gas))
+    print("Brake: {}".format(dynData.dynamicFollowData.brake))
