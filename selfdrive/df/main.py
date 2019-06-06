210db46dad070f1e23e59b1b98dc074aa0438638
Shane <shane@smiskol.com>
2019-06-06 13:35:01 -0500

    test

--- selfdrive/df/main.py
+++ selfdrive/df/main.py
@@ -6,8 +6,8 @@ start = time.time()
 #for i in range(50):
 model_output = libmpc.run_model(0.5768718769009631,
  0.46805728618371273,
- 0.41639219753371215,
+ 0.32030169041054785,
  0.21043000914913082,
- 0.5192517358961403)
+ 0.44063347852102075)
 #print(time.time() - start)
 print(model_output)
