9c28b01d4b1de1703b6ca9d7968c4eb311eeeb63
Shane Smiskol <shane@smiskol.com>
2019-06-06 14:39:05 -0500

    Update realtime.py

--- selfdrive/df/realtime.py
+++ selfdrive/df/realtime.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import selfdrive.messaging as messaging
 from selfdrive.services import service_list
 import zmq
+import sys
 
 context = zmq.Context()
 poller = zmq.Poller()
@@ -10,3 +11,4 @@ while True:
   dynData = messaging.recv_one(dynamic_follow_sock)
   if dynData is not None:
     print("Gas: {}\nBrake: {}".format(dynData.dynamicFollowData.gas, dynData.dynamicFollowData.brake), end="\r")
+    sys.stdout.flush()
