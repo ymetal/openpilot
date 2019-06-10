bbcfcf57c8698e1bf47752b03dd3ecd222cfdae6
Shane <shane@smiskol.com>
2019-06-10 18:18:18 -0500

    new model

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -300,7 +300,8 @@ def state_control(rcv_times, plan, path_plan, CS, CP, state, events, v_cruise_kp
       a_lead = lead_1.aLeadK
       has_lead = True
 
-  model_output = float(libmpc.run_model(norm(CS.vEgo, v_ego_scale), norm(CS.aEgo, a_ego_scale), norm(v_lead, v_lead_scale), norm(x_lead, x_lead_scale), norm(a_lead, a_lead_scale)))
+  #model_output = float(libmpc.run_model(norm(CS.vEgo, v_ego_scale), norm(CS.aEgo, a_ego_scale), norm(v_lead, v_lead_scale), norm(x_lead, x_lead_scale), norm(a_lead, a_lead_scale)))
+  model_output = float(libmpc.run_model(norm(CS.vEgo, v_ego_scale), norm(v_lead, v_lead_scale), norm(x_lead, x_lead_scale)))
 
   model_output = (model_output - 0.5) * 2.0
   #if has_lead:
