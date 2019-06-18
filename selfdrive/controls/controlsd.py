2df5b3caec34d52dbf4079d2c1d87db07351d400
Shane <shane@smiskol.com>
2019-06-18 17:43:56 -0500

    tuning

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -299,7 +299,7 @@ def state_control(rcv_times, plan, path_plan, CS, CP, state, events, v_cruise_kp
     #model_output = float(libmpc.run_model(norm(CS.vEgo, v_ego_scale), norm(CS.aEgo, a_ego_scale), norm(v_lead, v_lead_scale), norm(x_lead, x_lead_scale), norm(a_lead, a_lead_scale)))
     model_output = float(libmpc.run_model(norm(CS.vEgo, v_ego_scale), norm(v_lead, v_lead_scale), norm(x_lead, x_lead_scale), norm(a_lead, a_lead_scale)))
 
-    model_output = clip((model_output - 0.5) * 3.25, -1.0, 1.0)
+    model_output = clip((model_output - 0.55) * 3.5, -1.0, 1.0)
     #model_output = (model_output - 0.5) * 2.0
 
     actuators.gas = max(model_output, 0.0)
