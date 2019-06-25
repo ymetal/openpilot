cc3b70ae9a90d17a4262eeb0451786cb39d8e2b2
Shane <shane@smiskol.com>
2019-06-25 01:54:37 -0500

    new model trained by fkndean

--- selfdrive/df/dynamic_follow.cc
+++ selfdrive/df/dynamic_follow.cc
@@ -19,7 +19,7 @@ zdl::DlSystem::Runtime_t checkRuntime()
 
 void initializeSNPE(zdl::DlSystem::Runtime_t runtime) {
   std::unique_ptr<zdl::DlContainer::IDlContainer> container;
-  container = zdl::DlContainer::IDlContainer::open("/data/openpilot/selfdrive/df/gm-only.dlc");
+  container = zdl::DlContainer::IDlContainer::open("/data/openpilot/selfdrive/df/model3.dlc");
   //printf("loaded model\n");
   int counter = 0;
   zdl::SNPE::SNPEBuilder snpeBuilder(container.get());
@@ -64,9 +64,10 @@ zdl::DlSystem::ITensor* executeNetwork(std::unique_ptr<zdl::SNPE::SNPE>& snpe,
 }
 
 extern "C" {
-  float run_model(float v_ego, float v_lead, float x_lead, float a_lead){
+  float run_model(float v_ego, float a_ego, float v_lead, float x_lead, float a_lead){
     std::vector<float> inputVec;
     inputVec.push_back(v_ego);
+    inputVec.push_back(a_ego);
     inputVec.push_back(v_lead);
     inputVec.push_back(x_lead);
     inputVec.push_back(a_lead);
