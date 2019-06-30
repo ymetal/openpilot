d5b4ff88b6f4deb56be58f31113806cd65974816
Shane <shane@smiskol.com>
2019-06-30 16:18:23 -0500

    tests

--- selfdrive/df/df_wrapper.py
+++ selfdrive/df/df_wrapper.py
@@ -10,9 +10,9 @@ def get_wrapper():  # initialize df model and process long predictions
 
     ffi = FFI()
     ffi.cdef("""    
-    float run_model(float v_ego, float a_ego, float v_lead, float x_lead, float a_lead);
+    float run_model(float inputArray[100]);
     void init_model();
-    void test_input(float inputArray[100]);
+    void test_input(float array[20][5]);
     """)
 
     return ffi.dlopen(libmpc_fn)
