3394ec56f4d6c2b9d69a83de88c59f78f0e4946e
Shane <shane@smiskol.com>
2019-06-30 16:17:17 -0500

    tests

--- selfdrive/df/test.py
+++ selfdrive/df/test.py
@@ -11,6 +11,7 @@ start = time.time()
   0.28780443294609244,
   0.01075646532123655)
 print(time.time() - start)'''
+inputData = [1,2,3,4,5] * 20
 model_output = df_model.run_model(0.8653078153514447,
   0.46805728618371273,
   0.46805728618371273,
